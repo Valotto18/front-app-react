55a07055b188a50960099805d2ddbdfeaa933cb5
Valotto18 <rm551445@fiap.com.br>
2023-09-30 16:21:24 -0300

    Start do projeto live react + vite + json-server API.

---
+++ src/routes/Home.jsx
@@ -0,0 +1,14 @@
+export default function Home(){
+    return(
+      <section>
+        <div>
+          <p>CONHEÇA OS NOVOS GAMES DA ATUALIDADE</p>
+          <a href="#">
+              Conheça mais
+          </a>
+         
+        </div>
+    
+      </section>
+    )
+    }
---
+++ src/routes/Produtos.jsx
@@ -0,0 +1,10 @@
+export default function Produtos(){
+
+
+        return(
+            <section>
+                <h2>Produtos</h2>
+            </section>
+        )
+            
+}
